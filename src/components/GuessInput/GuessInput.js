dadca63d47b63b34f26e20926af804cb2c0b8cdd
Kai <2644614+Schweinepriester@users.noreply.github.com>
2024-02-22 21:06:31 +0000

    basic exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function GuessInput() {
+  const [guessInput, setGuessInput] = React.useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    console.log(guessInput.toUpperCase())
+    setGuessInput('')
+  }
+
+  return <form className="guess-input-wrapper" onSubmit={submitHandler}>
+    <label htmlFor="guess-input">Enter guess:</label>
+    <input 
+      id="guess-input"
+      type="text" 
+      value={guessInput}
+      pattern="\w{5,5}"
+      onChange={event => {
+        setGuessInput(event.target.value)
+      }}
+      />
+  </form>;
+}
+
+export default GuessInput;
